b2159c28cfc8bf01a78452a1f25444bdff962617
Vasiliy Maev <sweekom@gmail.com>
2022-09-28 16:27:38 +0300

    Generic task solved

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,3 +1,36 @@
-public class MagicBox {
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.Random;
+
+public class MagicBox<T> {
+
+    Random random = new Random();
+
+    public T[] items;
+
+    public MagicBox() {
+        this.items = (T[]) new Object[3];
+    }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException(String.format("Коробка не полна, добавь ещё " + (items.length - i)));
+            }
+        }
+        int randomInt = random.nextInt(3);
+        int index = randomInt;
+        return items[index];
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        MagicBox<String> magicBox = new MagicBox<>();
+        magicBox.add("Волшебная палочка");
+        magicBox.add("Книга заклинаний");
+
+
+        System.out.println(magicBox.pick());
+        magicBox.add("Зелье здоровья");
+        magicBox.add("Зелье маны");
 
     }
 }
